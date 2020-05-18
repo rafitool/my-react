776ff676bb0aa063dcdcce3a544ba2f7baa9f011
Hicham Rafi <>
2020-05-18 08:48:35 -0700

    changed table view to a component

--- src/mainLayout.js
+++ src/mainLayout.js
@@ -62,7 +62,7 @@ export default function ClippedDrawer() {
       </Drawer>
       <main className={classes.content}>
         <Toolbar />
-        {TableView()}
+        <TableView />
       </main>
     </div>
   );
--- src/tableView.js
+++ src/tableView.js
@@ -41,34 +41,75 @@ const tableIcons = {
 
 
 
-export default function TableView() {
-    
+class TableView extends React.Component {
+    constructor(props) {
 
-    return (
-        <div style={{ maxWidth: "100%" }}>
+        super(props);
+        this.state = {
+            columns: [
+                { title: 'Name', field: 'name' },
+                { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
+                { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+                {
+                    title: 'Birth Place',
+                    field: 'birthCity',
+                    lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+                },
+            ],
+            data: [
+                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+            ]
+        }
+    }
+
+    render() {
+        return (
             <MaterialTable
                 icons={tableIcons}
-                title="Selected Row Styling Preview"
-                columns={[
-                    { title: 'Name', field: 'name' },
-                    { title: 'Surname', field: 'surname' },
-                    { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-                    {
-                        title: 'Birth Place',
-                        field: 'birthCity',
-                        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-                    },
-                ]}
-                data={[
-                    { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-                    { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-                ]}
-                
-                
+                title="Editable Preview"
+                columns={this.state.columns}
+                data={this.state.data}
+                editable={{
+                    onRowAdd: newData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                {
+                                    const data = this.state.data;
+                                    data.push(newData);
+                                    this.setState({ data }, () => resolve());
+                                }
+                                resolve()
+                            }, 1000)
+                        }),
+                    onRowUpdate: (newData, oldData) =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                {
+                                    const data = this.state.data;
+                                    const index = data.indexOf(oldData);
+                                    data[index] = newData;
+                                    this.setState({ data }, () => resolve());
+                                }
+                                resolve()
+                            }, 1000)
+                        }),
+                    onRowDelete: oldData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                {
+                                    let data = this.state.data;
+                                    const index = data.indexOf(oldData);
+                                    data.splice(index, 1);
+                                    this.setState({ data }, () => resolve());
+                                }
+                                resolve()
+                            }, 1000)
+                        }),
+                }}
             />
-        </div>
-    );
+        )
+    }
 }
 
-
-
+export default TableView;
