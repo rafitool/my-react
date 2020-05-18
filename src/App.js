4b412bdac5d4b5ff88eb9c1d1143e7bed94bf560
Hicham Rafi <>
2020-05-18 09:42:17 -0700

    added form view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import ClippedDrawer from './mainLayout';
+import FormView from './formView';
 
 
 function App() {
   
     return ClippedDrawer();
+    //return (<FormView />);
   
 }
 
